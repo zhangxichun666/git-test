02aa83f4d93ceab610f850eeb7a4df3401b8c087
zhangxichun <javaweb1995@163.com>
2023-07-18 09:08:36 +0800

    first commit

---
+++ src/main/java/com/zxc/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.zxc.git;
+
+/**
+ * @ClassName GitTest
+ * @Description
+ * @Author LENOVO
+ * @Date 2023/7/18 9:02
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
