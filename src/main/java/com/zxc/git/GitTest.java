70d90c86ca23b0a15f5196eac2b6d75e3df45fb5
zhangxichun <javaweb1995@163.com>
2023-07-18 10:17:13 +0800

    push commit

--- src/main/java/com/zxc/git/GitTest.java
+++ src/main/java/com/zxc/git/GitTest.java
@@ -15,5 +15,6 @@ public class GitTest {
         System.out.println("hello git4");
         System.out.println("master test");
         System.out.println("hello git5");
+        System.out.println("push test");
     }
 }
