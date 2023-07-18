c6ec95185b6717a6eb3bc2853ebc86982cc433c8
ZhangXiChun <49582030+zhangxichun666@users.noreply.github.com>
2023-07-18 10:35:23 +0800

    pull commit

--- src/main/java/com/zxc/git/GitTest.java
+++ src/main/java/com/zxc/git/GitTest.java
@@ -16,5 +16,6 @@ public class GitTest {
         System.out.println("master test");
         System.out.println("hello git5");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
